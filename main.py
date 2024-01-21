cbd4562714fe332201a515a7e4e6bd24f1114e34
Leo <g_l_pochta@mail.ru>
2024-01-21 23:12:57 +0300

    flash fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session, redirect, url_for, render_template
+from flask import Flask, request, jsonify, session, redirect, url_for, render_template, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import session
