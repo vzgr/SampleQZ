cf4c7328f5bc485c03c40c0e72997f517a6ed4b9
Yarous <61277193+Yarous@users.noreply.github.com>
2024-01-21 21:00:56 +0300

    Fixed index

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import session
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9991secretkey'
@@ -36,13 +38,19 @@ def register():
             flash('Email address already exists')
             return redirect(url_for('register'))
 
-        new_user = User(email=email, password=generate_password_hash(password, method='sha256'))
+        new_user = User(email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful! Please login.')
         return redirect(url_for('login'))
     return render_template('Register.html')
 
+
+
+
+@app.route('/')
+def mainpage():
+    return render_template('Index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -51,17 +59,32 @@ def login():
         password = request.form.get('password')
 
         user = User.query.filter_by(email=email).first()
+
         if user and check_password_hash(user.password, password):
             flash('Login successful!')
+            print("you loged in!")
+            session['user_id'] = user.id
             return redirect(url_for('profile'))
         else:
             flash('Invalid login credentials!')
     return render_template('Login.html')
 
+@app.route('/logout')
+def logout():
+    # Удаляем id пользователя из сессии
+    session.pop('user_id', None)
+    flash('You have been logged out.')
+    return redirect(url_for('login'))
+
 
 @app.route('/profile')
 def profile():
-    return render_template('profile.html')
+    user_id = session.get('user_id')  # Получаем id пользователя из сессии
+    if user_id:
+        user = User.query.get(user_id)
+        if user:
+            return render_template('profile.html', user=user)
+    return redirect(url_for('login'))
 
 
 @app.route('/about')
