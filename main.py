b29ebae1778c5681e795d4113c1621758fa0196f
Yarous <61277193+Yarous@users.noreply.github.com>
2024-01-22 23:55:35 +0300

    Server reverb, all working!

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ app.config['SECRET_KEY'] = '9991secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-
 first_request_initialized = False
 
 @app.before_request
 def before_request():
     global first_request_initialized
     if not first_request_initialized:
-        # Ваш код инициализации здесь
         db.create_all()
         first_request_initialized = True
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    unitytest = db.Column(db.String(100))
+    pytest = db.Column(db.String(100))
+    jstest = db.Column(db.String(100))
 
 
 
@@ -32,13 +33,12 @@ def register():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
-
         user = User.query.filter_by(email=email).first()
         if user:
             flash('Email address already exists')
             return redirect(url_for('register'))
 
-        new_user = User(email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
+        new_user = User(email=email, password=generate_password_hash(password, method='pbkdf2:sha256'), unitytest=0, pytest=0, jstest=0)
         db.session.add(new_user)
         db.session.commit()
         flash('Registration successful! Please login.')
@@ -72,33 +72,59 @@ def login():
 
 @app.route('/logout')
 def logout():
-    # Удаляем id пользователя из сессии
     session.pop('user_id', None)
     flash('You have been logged out.')
     return redirect(url_for('login'))
 
 
-@app.route('/receive-data', methods=['POST'])
-def receive_data():
-    data = request.json  # Flask автоматически преобразует JSON-данные из POST-запроса
-    jsVariable = data['myVar']  # Извлекаем переменную из данных
-    print("Полученная переменная:", jsVariable)
+@app.route('/process', methods=['POST'])
+def process():
+    if 'user_id' not in session:
+        return jsonify({"error": "Unauthorized access"}), 401
 
+    user_id = session['user_id']
+    print(user_id)
+    data = request.get_json(force=True)
 
+    page = data.get('page')
+    variable = data.get('variable')
 
-    # Отправляем ответ обратно в JavaScript
-    return jsonify(success=True, message="Данные получены")
+    if not page or variable is None:
+        return jsonify({"error": "Missing 'page' or 'variable' data"}), 400
+
+    if hasattr(User, page):
+        user = db.session.get(User, user_id)
+        print(user)
+        if user:
+            setattr(user, page, variable)
+            db.session.commit()
+            print("commited!")
+            return jsonify({"success": f"Column {page} updated with value {variable} for user {user_id}."})
+        else:
+            print("dont commited!")
+            return jsonify({"error": "User not found"}), 404
+    else:
+        return jsonify({"error": f"Column {page} does not exist in User model"}), 400
 
 
 @app.route('/profile')
 def profile():
-    is_authenticated = 'user_id' in session  # здесь будит True, если пользователь авторизован
-    user_id = session.get('user_id')  # Получаем id пользователя из сессии
-    if user_id:
-        user = User.query.get(user_id)
+    is_authenticated = 'user_id' in session
+    if is_authenticated:
+        user_id = session.get('user_id')
+        user = db.session.get(User, user_id)
+
         if user:
-            return render_template('profile.html', user=user, is_authenticated=is_authenticated)
-    return redirect(url_for('login'))
+            utest = user.unitytest
+            jtest = user.jstest
+            ptest = user.pytest
+            return render_template('profile.html', user=user, is_authenticated=is_authenticated, javastest=jtest, pythtest=ptest, unitygametest=utest)
+        else:
+            flash('User not found.')
+            return redirect(url_for('login'))
+    else:
+        flash('You are not logged in.')
+        return redirect(url_for('login'))
 
 
 @app.route('/about')
